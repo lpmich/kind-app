92d01c1b957053c00892c4f9bd3ebee159541ecf
Logan Michalak <logan.michalak@sas.com>
2022-06-30 15:50:33 -0400

    Remove db functionality out of main
    Unstable: Do not run project off this commit

--- main.go
+++ main.go
@@ -1,17 +1,13 @@
 package main
 
 import (
-    "os"
     "log"
     "fmt"
-    "time"
     "net/http"
     "text/template"
     "database/sql"
     "github.com/go-sql-driver/mysql"
 )
-
-var db *sql.DB
 
 type Person struct {
     First string
@@ -19,63 +15,21 @@ type Person struct {
     Color string
 }
 
-// Create database
-func createDB() error {
-    cfg := mysql.Config{
-        User: "root",
-        Passwd: os.Getenv("MYSQL_ROOT_PASSWORD"),
-        Net: "tcp",
-        Addr: os.Getenv("MYSQL_URL")+":3306"}
-    var err error
-    db, err = sql.Open("mysql", cfg.FormatDSN())
-    if err != nil {
-        log.Fatal("Can't connect to database: %v", err)
-    }
-    db.Exec("CREATE DATABASE people;")
-    db.Exec("USE people")
-    query := `CREATE TABLE IF NOT EXISTS people(first VARCHAR(50) NOT NULL,
-              last VARCHAR(50) NOT NULL, color VARCHAR(50) NOT NULL,
-              id INTEGER AUTO_INCREMENT, PRIMARY KEY (id))`
-    _, err = db.Exec(query)
-    if err != nil {
-        return err
-    }
-    fmt.Println("Database Connected!")
-    return nil
-}
-
-// Retrieve all people in database
-func getPeople()([]Person, error) {
-    var people []Person
-
-    rows, err := db.Query("SELECT first, last, color FROM people")
-    if err != nil {
-        return nil, fmt.Errorf("Error retrieving from database: ", err)
-    }
-    defer rows.Close()
-    for rows.Next() {
-        var person Person
-        if err := rows.Scan(&person.First, &person.Last, &person.Color); err != nil {
-            return nil, fmt.Errorf("Error reading data: ", err)
-        }
-        people = append(people, person)
-    }
-
-    return people, nil
-}
-
-// Add a person to database
-func addPerson(person Person) {
-    _, err := db.Exec("INSERT INTO people (first, last, color) VALUES (?, ?, ?)",
-        person.First, person.Last, person.Color)
-    if err != nil {
-        log.Fatal("Error inserting into database: %v", err)
-    }
+// Redirects http to https
+func redirectHTTP(w http.ResponseWriter, r *http.Request) {
+    http.Redirect(w, r, "https://localhost"+r.RequestURI, 302)
 }
 
 // Serve index.html
 func indexHandler(w http.ResponseWriter, r *http.Request) {
     http.ServeFile( w, r, "assets/index.html")
+}
+
+// Serve view.html
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+    people, _ := db.Getpeople()
+    t, _ := template.ParseFiles("assets/view.html")
+    t.Execute(w, people)
 }
 
 // Process input from form
@@ -90,11 +44,11 @@ func processHandler(w http.ResponseWriter, r *http.Request) {
     color := r.FormValue("color")
 
     // Add person to database
-    var person Person
+    var person db.Person
     person.First = fname
     person.Last = lname
     person.Color = color
-    addPerson(person)
+    db.Addperson(person)
     http.Redirect(w, r, "/view", 303)
 }
 
@@ -112,25 +66,17 @@ func redirectHTTP(w http.ResponseWriter, r *http.Request) {
 
 // Serve application
 func main() {
+
     fmt.Println("Starting Application...")
 
-    // Try to connect to database 10 times
-    var err error
-    for i:= 0; i < 10; i++ {
-        if i > 0 {
-           log.Println("Attempting to connect to Database: ", i)
-           time.Sleep(10 * time.Second)
-        }
-        err = createDB()
-        if err == nil {
-            break
-        }
-    }
+    // Connect to database
+    err := db.Conn()
     if err != nil {
         log.Fatal(err)
     }
-    // Listen and handle http/s requests
-    fmt.Println("Serving Application")
+
+    // Listen for http/s requests
+    fmt.Println("Serving Application...")
     http.HandleFunc("/", indexHandler)
     http.HandleFunc("/process", processHandler)
     http.HandleFunc("/view", viewHandler)
