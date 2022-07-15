b589bd2b58296faa92acf2056d3f2361ce14e0be
Logan Michalak <logan.michalak@sas.com>
2022-07-15 14:40:59 -0400

    Enable JWT token creation and authentication for API

--- api/api.go
+++ api/api.go
@@ -2,11 +2,14 @@ package api
 
 import (
     "fmt"
+    "time"
     "net/http"
+    "strings"
+    "encoding/json"
     "gitlab.sas.com/lomich/kind-app/db"
     "gitlab.sas.com/lomich/kind-app/security"
     "github.com/gin-gonic/gin"
-//    "github.com/golang-jwt/jwt"
+    "github.com/golang-jwt/jwt"
 )
 
 type post struct {
@@ -18,22 +21,86 @@ type post struct {
     Id  int `json:"id"`
 }
 
-// Initialize GIN API and expose endpoints
-func StartAPI() {
-    router := gin.Default()
-    router.GET("", apiLanding)
-    router.GET("/api/post/:id", getPost)
-    router.GET("/api/posts", getPosts)
-    router.POST("api/post", postPost)
-    router.GET("/api/comments", getComments)
-    router.RunTLS(":8080", "security/server.crt", "security/server.key")
+type credentials struct {
+    Username string `json:"username"`
+    Password string `json:"password"`
+}
+
+type claims struct {
+    Username string `json:"username"`
+    jwt.StandardClaims
+}
+
+var signingKey = []byte("pvh7YQqceEC0qqKUSCTh")
+
+// Generates a new JWT
+func generateJWT(c *gin.Context) {
+    var creds credentials
+
+    // Decode body and read it into a credentials object
+    err := json.NewDecoder(c.Request.Body).Decode(&creds)
+    if err != nil {
+        fmt.Println(err)
+        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        return
+    }
+
+    // Check user is verified
+    _, err = security.Authenticate(creds.Username, creds.Password)
+    if err != nil {
+        fmt.Println(err)
+        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        return
+    }
+
+    // Make JWT
+    expirationTime := time.Now().Add(time.Hour * 168)
+    claim := &claims {
+        Username: creds.Username,
+        StandardClaims: jwt.StandardClaims {
+            ExpiresAt: expirationTime.Unix()},
+    }
+    token := jwt.NewWithClaims(jwt.SigningMethodHS512, claim)
+    tokenString, err := token.SignedString(signingKey)
+    if err != nil {
+        fmt.Println(err)
+        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        return
+    }
+
+     m := make(map[string]string)
+     m["key"] = tokenString
+     jsonString, _ := json.Marshal(m)
+    c.JSON(http.StatusOK, jsonString)
 }
 
 // Determines if a request is authorized
 func authorized(c *gin.Context) bool {
     authHeader := c.Request.Header["Authorization"]
     if len(authHeader) > 0 {
-//        jwt := strings.Fields(authHeader[0])[1]
+        jwtString := strings.Fields(authHeader[0])[1]
+        claims := &claims{}
+        tkn, err := jwt.ParseWithClaims(jwtString, claims,
+            func(t *jwt.Token) (interface{}, error) {
+                return signingKey, nil
+            })
+        if err != nil {
+            fmt.Println()
+            if err == jwt.ErrSignatureInvalid {
+                c.JSON(http.StatusUnauthorized, gin.H{
+                    "error": err.Error()})
+                return false
+            }
+            c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+            return false
+        }
+        if !tkn.Valid {
+            fmt.Println()
+            c.JSON(http.StatusUnauthorized, gin.H{"error":
+                "jwt is not valid"})
+            return false
+        }
+        return true
     } else {
         cookie, _ := c.Request.Cookie("sessionid")
         if cookie == nil {
@@ -59,7 +126,9 @@ func getPost(c *gin.Context) {
     id := c.Param("id")
     db_post, err := db.GetPost(id)
     if err != nil {
+        fmt.Println(err)
         c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        return
     }
 
     post.Content = db_post.Content
@@ -100,4 +169,18 @@ func postPost(c *gin.Context) {
 // Get all comments in the system
 func getComments(c *gin.Context) {
     c.IndentedJSON(http.StatusOK, nil)
+
 }
+
+// Initialize GIN API and expose endpoints
+func StartAPI() {
+    router := gin.Default()
+    router.GET("", apiLanding)
+    router.POST("/api/jwt", generateJWT)
+    router.GET("/api/post/:id", getPost)
+    router.GET("/api/posts", getPosts)
+    router.POST("api/post", postPost)
+    router.GET("/api/comments", getComments)
+    router.RunTLS(":8080", "security/server.crt", "security/server.key")
+}
+
